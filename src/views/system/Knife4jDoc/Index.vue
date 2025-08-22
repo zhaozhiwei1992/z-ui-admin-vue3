b6381dc34cabfae47cca60c2478f7ae5c8f81608
zhaozhiwei <347061329@qq.com>
2025-08-22 21:31:09 +0800

    fix: 文档样式调整

--- src/views/system/Knife4jDoc/Index.vue
+++ src/views/system/Knife4jDoc/Index.vue
@@ -1,12 +1,18 @@
 <template>
   <ContentWrap>
-    <IFrame :src="src" height="700" width="100%" />
+    <IFrame :src="src" width="100%" />
   </ContentWrap>
 </template>
 <script setup lang="ts" name="SwaggerIndex">
-import { ref } from 'vue'
 import { getAccessToken } from '@/utils/auth'
+import { ref } from 'vue'
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL
 const src = ref(BASE_URL + '/doc.html?token=' + getAccessToken())
 </script>
+<style scoped lang="scss">
+iframe {
+  border: none;
+  height: calc(100vh - 80px - 40px);
+}
+</style>
